18317d15bf17f8acd682aa65bd783de6cf04d9d2
Bruno DaSilva <hi.dasilvab@gmail.com>
2021-02-05 13:01:22 -0600

    Added nnew name to DB

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const db = mongoose.connection;
 
 //ENVIRONMENT VARIABLES
 const mongoURI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/merncrud";
+  process.env.MONGODB_URI || "mongodb://localhost:27017/merncrudbookmark";
 const PORT = process.env.PORT || 3001;
 
 //CONNECT TO MONGO
